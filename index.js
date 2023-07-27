468cc784b626995791f9823e63aee9b3a791fad3
gautamarora07 <139057023+gautamarora07@users.noreply.github.com>
2023-07-27 18:40:58 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,54 @@
-var r1 = Math.floor(Math.random()*6) + 1;
-var r2 = Math.floor(Math.random()*6) + 1;
+var randomNo1 = Math.floor(Math.random()*6) + 1;
+var randomNo1Img = "Images/dice" + randomNo1 + ".png";
+document.querySelector("#dice1 img").setAttribute("src", randomNo1Img);
 
+
+var randomNo2 = Math.floor(Math.random()*6) + 1;
+var randomNo2Img = "Images/dice" + randomNo2 + ".png";
+document.querySelector("#dice2 img").setAttribute("src", randomNo2Img);
+
+if(randomNo1>randomNo2)
+{
+document.querySelector(".result").innerHTML = "🎉PLAYER 1 WINS!";
+}
+else if(randomNo1<randomNo2)
+{
+    document.querySelector(".result").innerHTML = "PLAYER 2 WINS!🎉";
+}
+else
+{
+    document.querySelector(".result").innerHTML = "DRAW!";
+}
+
+
+function reset()
+{
+   var resultP =  document.querySelector(".result");
+   resultP.innerHTML = "Let's 🎲 Begin";
+   resultP.style.backgroundColor = "black";
+   resultP.style.color  = "cornflowerblue"
+ 
+}
+
+/*
 const P1dice = document.querySelector("#dice1 img");
 const P2dice = document.querySelector("#dice2 img");
 
 if(r1>r2)
+{
 document.querySelector(".result").innerHTML = "🎉PLAYER 1 WINS!";
+//document.querySelector("#dice1").style.backgroundColor = "Grey";
+//document.querySelector("#dice1").style.borderRadius = "10%";
+
+}
 else if(r1<r2)
-document.querySelector(".result").innerHTML = "🎉PLAYER 2 WINS!";
+{
+    document.querySelector(".result").innerHTML = "🎉PLAYER 2 WINS!";
+}
 else
-document.querySelector(".result").innerHTML = "😶DRAW!";
+{
+    document.querySelector(".result").innerHTML = "😶DRAW!";
+}
 
 if(r1 === 1 )
     {
@@ -65,5 +104,7 @@ else
 
     function reset()
     {
-        document.querySelector(".result").innerHTML = "LET's BEGIN";
-    }
+       var resultP =  document.querySelector(".result");
+       resultP.innerHTML = "LET's BEGIN";
+       resultP.style.backgroundColor = "none";
+    }*/
